9f093c9a0318ecaa5c08af21f07c12f64a2b98c6
Hilton1 <josehilton02@gmail.com>
2023-01-19 10:46:07 -0300

    Criando o componente Main

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Main } from './src/Main';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
     'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
-    'GeneralSans-500': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
-    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
   });
 
   if(!isFontsLoaded) {
@@ -14,18 +13,6 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'GeneralSans-400' }}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

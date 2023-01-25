fb80dc9a433be3fd95c0a027dc702abb771d49cd
Hilton1 <josehilton02@gmail.com>
2023-01-25 16:22:46 -0300

    Criando o componente Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,34 @@
+import { FlatList } from 'react-native';
+
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+
+import { Product, ProductImage, ProductDetails } from './styles';
+
 export function Menu() {
   return (
-    null
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{paddingHorizontal: 24}}
+      keyExtractor={product => product._id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.5.154:3001/uploads/${product.imagePath}`
+            }}
+          />
+
+          <ProductDetails>
+            <Text weight='600'>{product.name}</Text>
+            <Text size={14} color='#666' style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight='600'>{product.price}</Text>
+          </ProductDetails>
+        </Product>
+      )}
+    />
   );
 }
